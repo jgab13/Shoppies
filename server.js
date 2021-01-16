2f486c8a23aedf1217d17720e1e5bce7e2efc530
gabejona <jonathan.gabe@mail.utoronto.ca>
2021-01-16 10:47:27 -0500

    Adding server

---
+++ server.js
@@ -0,0 +1,56 @@
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+// Setting up a static directory for the files in /pub
+// using Express middleware.
+// Don't put anything in /pub that you don't want the public to have access to!
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+const bodyParser = require('body-parser') 
+// app.use(bodyParser.json())
+// Need for profile pictures
+const limit = "50mb"
+app.use(bodyParser.json({limit: limit, extended: true}))
+app.use(bodyParser.urlencoded({limit: limit, extended: true}))
+
+// express-session for managing user sessions
+const session = require("express-session");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/*** Session handling **************************************/
+// Create a session and session cookie
+app.use(
+    session({
+        secret: "our hardcoded secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 6000000,
+            httpOnly: true,
+            sameSite: "strict"
+        }
+    })
+);
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+    // send index.html
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+
+// will use an 'environmental variable', process.env.PORT, for deployment.
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+})  // localhost development port 5000  (http://localhost:5000)
+   // We've bound that port to localhost to go to our express server.
+   // Must restart web server when you make changes to route handlers.
+
